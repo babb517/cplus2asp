b189942acb632da04dc823c9ec276174745d949e
Joseph Babb <jbabb1@asu.edu>
2013-03-22 17:56:28 -0700

    Added additional wrapping and error checking to check for and (attempt to) maintain the reference to as2transition's repository.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,44 @@
 #! /bin/bash
 
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CALL_PATH="$PWD"
+
+
+# Make sure as2transition is there or try to get it.
+if [ ! -e "$SCRIPT_PATH/externals/as2transition/bootstrap.sh" ]
+then
+	echo "Warning: The as2transition submodule has not been initialized." 1>&2
+	echo "Attempting to initialize as2transition"
+	if hash git 2>/dev/null
+	then
+		cd "$SCRIPT_PATH"
+		git submodule init
+		git submodule update
+		cd "$CALL_PATH"
+
+	else
+		echo "Error: Failed to initialize as2transition. Git not found." 1>&2
+		echo "Error: Please install Git and try again." 1>&2
+	
+	fi
+fi
+
 # recursive bootstrapping
-echo "Bootstrapping as2transition"
+if [ -e "$SCRIPT_PATH/externals/as2transition/bootstrap.sh" ]
+then
+	echo "Bootstrapping as2transition"
+	cd "$SCRIPT_PATH/externals/as2transition"
+	./bootstrap.sh noconf
+	cd "$CALL_PATH"
+else
+	echo "Error: as2transition is required to run the cplus2asp toolchain." 1>&2
+	echo "Error: Please download and install as2transition from 'https://github.com/babb517/as2transition'." 1>&2
+fi
 
-cd externals/as2transition
-./bootstrap.sh noconf
-cd ../..
 
-echo "Bootstrapping cplus2asp"
-
-# perform the bootstrap passing arguments to autoconf
-
+# perform the bootstrap 
+echo "Bootstrapping cplus2asp..."
+cd "$SCRIPT_PATH"
 if [ "$1" == "noconf" ]
 then
 
@@ -28,5 +56,5 @@ else
 		autoconf && \
 		./configure "$@"
 fi
-
+cd "$CALL_PATH"
 
